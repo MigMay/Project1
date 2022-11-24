fddcbb08d4fb1ee2c204d5b15dabdca7ed4c60e7
Gabe Arenas <garenas@unomaha.edu>
2022-11-23 23:03:39 -0600

    Set up vote menu

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from PyQt5.QtWidgets import QApplication, QWidget
-from votemenu import Ui_VoteMenu
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
+from PyQt5.QtCore import Qt
 
 
 class VoteMenu(QWidget):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_VoteMenu()
-        self.ui.setupUi(self)
+        self.setUpWidgets()
+
+    def setUpWidgets(self):
+        """Set up widgets in window."""
+        intro_label = QLabel("VOTE MENU")
+        vote_button = QPushButton("vote")
+        exit_button = QPushButton("exit")
+        exit_button.clicked.connect(self.close)
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(intro_label, alignment=Qt.AlignmentFlag.AlignHCenter)
+        main_layout.addWidget(vote_button)
+        main_layout.addWidget(exit_button)
+        self.setLayout(main_layout)
 
 
 if __name__ == "__main__":
