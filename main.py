c734e024180c4f4304e29ce03d9e34e3a77c7cf4
Gabe Arenas <garenas@unomaha.edu>
2022-11-23 23:09:03 -0600

    Add PickCandidate

--- main.py
+++ main.py
@@ -5,18 +5,37 @@ from PyQt5.QtCore import Qt
 class VoteMenu(QWidget):
     def __init__(self):
         super().__init__()
+        self.candidate_window = PickCandidate()
         self.setUpWidgets()
 
     def setUpWidgets(self):
         """Set up widgets in window."""
         intro_label = QLabel("VOTE MENU")
         vote_button = QPushButton("vote")
+        vote_button.clicked.connect(self.candidate_window.show)
         exit_button = QPushButton("exit")
         exit_button.clicked.connect(self.close)
         main_layout = QVBoxLayout()
         main_layout.addWidget(intro_label, alignment=Qt.AlignmentFlag.AlignHCenter)
         main_layout.addWidget(vote_button)
         main_layout.addWidget(exit_button)
+        self.setLayout(main_layout)
+
+
+class PickCandidate(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setUpWidgets()
+
+    def setUpWidgets(self):
+        """Set up widgets in window."""
+        candidates_label = QLabel("CANDIDATES")
+        john_button = QPushButton("John")
+        jane_button = QPushButton("Jane")
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(candidates_label, alignment=Qt.AlignmentFlag.AlignHCenter)
+        main_layout.addWidget(john_button)
+        main_layout.addWidget(jane_button)
         self.setLayout(main_layout)
 
 
