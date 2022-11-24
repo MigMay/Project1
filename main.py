95605c46ed8a3520d00850b41ec963ee8d4ab022
Gabe Arenas <garenas@unomaha.edu>
2022-11-23 22:54:30 -0600

    Copy gui.py contents into main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from view import *
+from PyQt5.QtWidgets import QApplication, QWidget
+from votemenu import Ui_VoteMenu
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
 
+class VoteMenu(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_VoteMenu()
+        self.ui.setupUi(self)
 
 
 if __name__ == "__main__":
     import sys
-    main()
+    app = QApplication(sys.argv)
+    menu = VoteMenu()
+    menu.show()
+    sys.exit(app.exec_())
