5c1335a82d14106e1f9175733b11175dc8fb25a3
Farosh <46266489+PhotogenicLizard@users.noreply.github.com>
2022-11-27 14:40:37 -0600

    add files

---
+++ votesubmitted.py
@@ -0,0 +1,48 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_VoteSubmitted(object):
+    def setupUi(self, VoteSubmitted):
+        VoteSubmitted.setObjectName("VoteSubmitted")
+        VoteSubmitted.resize(497, 399)
+        self.label_13 = QtWidgets.QLabel(VoteSubmitted)
+        self.label_13.setGeometry(QtCore.QRect(40, 50, 391, 191))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label_13.setFont(font)
+        self.label_13.setWordWrap(True)
+        self.label_13.setObjectName("label_13")
+        self.label_14 = QtWidgets.QLabel(VoteSubmitted)
+        self.label_14.setGeometry(QtCore.QRect(40, 0, 391, 141))
+        font = QtGui.QFont()
+        font.setPointSize(22)
+        self.label_14.setFont(font)
+        self.label_14.setWordWrap(True)
+        self.label_14.setObjectName("label_14")
+        self.pushButton_3 = QtWidgets.QPushButton(VoteSubmitted)
+        self.pushButton_3.setGeometry(QtCore.QRect(130, 240, 200, 50))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(VoteSubmitted)
+        self.pushButton_4.setGeometry(QtCore.QRect(130, 300, 200, 50))
+        self.pushButton_4.setObjectName("pushButton_4")
+
+        self.retranslateUi(VoteSubmitted)
+        QtCore.QMetaObject.connectSlotsByName(VoteSubmitted)
+
+    def retranslateUi(self, VoteSubmitted):
+        _translate = QtCore.QCoreApplication.translate
+        VoteSubmitted.setWindowTitle(_translate("VoteSubmitted", "Dialog"))
+        self.label_13.setText(_translate("VoteSubmitted", "Voter ID voted for Candidate"))
+        self.label_14.setText(_translate("VoteSubmitted", "Your vote was submitted!"))
+        self.pushButton_3.setText(_translate("VoteSubmitted", "Start Another Vote"))
+        self.pushButton_4.setText(_translate("VoteSubmitted", "See Results"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    VoteSubmitted = QtWidgets.QDialog()
+    ui = Ui_VoteSubmitted()
+    ui.setupUi(VoteSubmitted)
+    VoteSubmitted.show()
+    sys.exit(app.exec_())
---
+++ welcomemenu.py
@@ -0,0 +1,48 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_WelcomeMenu(object):
+    def setupUi(self, WelcomeMenu):
+        WelcomeMenu.setObjectName("WelcomeMenu")
+        WelcomeMenu.resize(500, 600)
+        self.pushButton = QtWidgets.QPushButton(WelcomeMenu)
+        self.pushButton.setGeometry(QtCore.QRect(140, 370, 200, 50))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(WelcomeMenu)
+        self.pushButton_2.setGeometry(QtCore.QRect(140, 430, 200, 50))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label = QtWidgets.QLabel(WelcomeMenu)
+        self.label.setGeometry(QtCore.QRect(40, 30, 391, 141))
+        font = QtGui.QFont()
+        font.setPointSize(22)
+        self.label.setFont(font)
+        self.label.setWordWrap(True)
+        self.label.setObjectName("label")
+        self.label_3 = QtWidgets.QLabel(WelcomeMenu)
+        self.label_3.setGeometry(QtCore.QRect(40, 90, 391, 191))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label_3.setFont(font)
+        self.label_3.setWordWrap(True)
+        self.label_3.setObjectName("label_3")
+
+        self.retranslateUi(WelcomeMenu)
+        QtCore.QMetaObject.connectSlotsByName(WelcomeMenu)
+
+    def retranslateUi(self, WelcomeMenu):
+        _translate = QtCore.QCoreApplication.translate
+        WelcomeMenu.setWindowTitle(_translate("WelcomeMenu", "Dialog"))
+        self.pushButton.setText(_translate("WelcomeMenu", "Begin Voting"))
+        self.pushButton_2.setText(_translate("WelcomeMenu", "Log off"))
+        self.label.setText(_translate("WelcomeMenu", "Vote For Your Next Student Body President"))
+        self.label_3.setText(_translate("WelcomeMenu", "Universtity of Nebraska at Omaha has begun elections for their student body president for 2023/2024"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    WelcomeMenu = QtWidgets.QDialog()
+    ui = Ui_WelcomeMenu()
+    ui.setupUi(WelcomeMenu)
+    WelcomeMenu.show()
+    sys.exit(app.exec_())
