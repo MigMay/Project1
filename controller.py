03ae4072e322cc6fd48ca72d78ce805024248f23
Gabe Arenas <garenas@unomaha.edu>
2022-11-27 22:21:39 -0600

    Revise MainController to WelcomeMenuController

--- controller.py
+++ controller.py
@@ -3,19 +3,16 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from welcomemenu import Ui_WelcomeMenu
 
 
-class MainController:
-    def __init__(self):
-        self.welcome_menu = QWidget()
-        self.setupUiForAllWindows()
-        self.welcome_menu.show()
-
-    def setupUiForAllWindows(self):
-        ui = Ui_WelcomeMenu()
-        ui.setupUi(self.welcome_menu)
+class WelcomeMenuController(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ui = Ui_WelcomeMenu()
+        self.ui.setupUi(self)
+        self.show()
 
 
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
-    controller = MainController()
+    controller = WelcomeMenuController()
     sys.exit(app.exec_())
