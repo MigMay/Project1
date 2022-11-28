3ec802bbde60ec0859c69cca1acd38982237bee0
Gabe Arenas <garenas@unomaha.edu>
2022-11-27 22:40:40 -0600

    Add VoteSubmitted window

--- controller.py
+++ controller.py
@@ -2,17 +2,20 @@ from PyQt5.QtWidgets import QApplication, QWidget
 
 from welcomemenu import Ui_WelcomeMenu
 from votemenu import Ui_VoteMenu
+from votesubmitted import Ui_VoteSubmitted
 
 
 class MainController:
     def __init__(self):
         self.welcome_menu = WelcomeMenu()
         self.candidate_menu = CandidateMenu()
+        self.vote_submitted = VoteSubmitted()
         self.set_up_signals_and_slots()
         self.welcome_menu.show()
 
     def set_up_signals_and_slots(self):
         self.welcome_menu.ui.pushButton.clicked.connect(self.candidate_menu.show)
+        self.candidate_menu.ui.pushButton_9.clicked.connect(self.vote_submitted.show)
 
 
 class WelcomeMenu(QWidget):
@@ -34,6 +37,17 @@ class CandidateMenu(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.ui = Ui_VoteMenu()
+        self.ui.setupUi(self)
+        self.setUpSignalsAndSlots()
+
+    def setUpSignalsAndSlots(self):
+        pass
+
+
+class VoteSubmitted(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ui = Ui_VoteSubmitted()
         self.ui.setupUi(self)
         self.setUpSignalsAndSlots()
 
