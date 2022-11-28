655499d630410a0c619f2675a6cc284f9c879d81
Gabe Arenas <garenas@unomaha.edu>
2022-11-27 22:08:46 -0600

    Create MainController

--- controller.py
+++ controller.py
@@ -3,11 +3,19 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from welcomemenu import Ui_WelcomeMenu
 
 
+class MainController:
+    def __init__(self):
+        self.welcome_menu = QWidget()
+        self.setupUiForAllWindows()
+        self.welcome_menu.show()
+
+    def setupUiForAllWindows(self):
+        ui = Ui_WelcomeMenu()
+        ui.setupUi(self.welcome_menu)
+
+
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
-    welcomemenu = QWidget()
-    ui_welcomemenu = Ui_WelcomeMenu()
-    ui_welcomemenu.setupUi(welcomemenu)
-    welcomemenu.show()
+    controller = MainController()
     sys.exit(app.exec_())
