829258715200bc137a470df511e9ecd2b1c10017
Farosh <46266489+PhotogenicLizard@users.noreply.github.com>
2022-12-03 20:14:14 -0600

    able to read from list

--- helper.py
+++ helper.py
@@ -5,17 +5,29 @@ this file will manage the csv file only
 store and retrieve data from here
 
 """
-
 import csv
+from results import *
 
-import
 
-
+"""
 class resultsHelper():
     def importVotes(self):
-        pass
+    
+
+        #Count the separate occurrences of votes.
+        result = {}
+        with open('votingrecords2.csv', 'r', newline='') as csv_file:
+            csv_reader = csv.reader(csv_file)
+
+            for line in csv_file:
+                print(line[0])
+
+    #return result
+
 
     def changeWinnerData(self):
+        Ui_Results.retranslateUi().self.label_18.setText(str())
+
 
 
 jv=1
@@ -33,4 +45,26 @@ class candidateMenuHelper():
             csv_writer.writeheader()
             csv_writer.writerow()
 
-candidateMenuHelper.logVote()
+#candidateMenuHelper.logVote()
+
+
+
+resultsHelper.importVotes()
+
+"""
+
+CA_vote = 0
+CB_vote = 0
+CC_vote = 0
+
+with open('votingrecords2.csv', 'r', newline='') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    for row in csv_reader:
+        CA_vote += int(row[0].strip())
+        CB_vote += int(row[1].strip())
+        CC_vote += int(row[2].strip())
+
+    print(CA_vote)
+    print(CB_vote)
+    print(CC_vote)
