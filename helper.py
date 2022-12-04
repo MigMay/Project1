8ba56995158fffc17cf40066cd76d45c5026833d
Farosh <46266489+PhotogenicLizard@users.noreply.github.com>
2022-12-03 22:46:56 -0600

    removed the classes

--- helper.py
+++ helper.py
@@ -4,57 +4,47 @@ this file will manage the csv file only
 
 store and retrieve data from here
 
+once start another, rewrite the csv file.
+
+testing
+from welcom
+if no data in csv, then raise exception
+- try exept handler in controller
+
 """
 import csv
 from results import *
 
+def readVotes():
+    CA_vote = 0
+    CB_vote = 0
+    CC_vote = 0
 
-class resultsHelper():
+    with open('votingrecords2.csv', 'r', newline='') as csv_file:
+        csv_reader = csv.reader(csv_file)
 
-    def readVotes():
-            CA_vote = 0
-            CB_vote = 0
-            CC_vote = 0
+        for row in csv_reader:
+            CA_vote += int(row[0].strip())
+            CB_vote += int(row[1].strip())
+            CC_vote += int(row[2].strip())
 
-            with open('votingrecords2.csv', 'r', newline='') as csv_file:
-                csv_reader = csv.reader(csv_file)
+        print(CA_vote)
+        print(CB_vote)
+        print(CC_vote)
 
-                for row in csv_reader:
-                    CA_vote += int(row[0].strip())
-                    CB_vote += int(row[1].strip())
-                    CC_vote += int(row[2].strip())
-
-                print(CA_vote)
-                print(CB_vote)
-                print(CC_vote)
-
-            return CA_vote, CB_vote, CC_vote
-
-    def updateResults(CA_vote, CB_vote, CC_vote ):
-        Ui_Results.retranslateUi().self.label_18.setText(str(CA_vote))
-        Ui_Results.retranslateUi().self.label_18.setText(str(CB_vote))
-        Ui_Results.retranslateUi().self.label_18.setText(str(CC_vote))
+    return CA_vote, CB_vote, CC_vote
 
 
+def logVote():
+    with open('votingrecords.csv', 'w', newline='') as csv_file:
+        fieldnames = ['Jeane', 'Edwin', 'Patricia']
+        csv_reader = csv.reader(csv_file)
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-
-jv=1
-ev=2
-pv=3
-
-class candidateMenuHelper():
-
-    def logVote():
-        with open('votingrecords.csv', 'w', newline='') as csv_file:
-            fieldnames = ['Jeane', 'Edwin', 'Patricia']
-            csv_reader = csv.reader(csv_file)
-            csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-            csv_writer.writeheader()
-            csv_writer.writerow()
+        csv_writer.writeheader()
+        csv_writer.writerow()
 
 def main():
-    resultsHelper.readVotes()
-    resultsHelper.updateResults()
+    var, var2, var3 = readVotes()
 
 main()
