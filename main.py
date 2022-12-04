be627f03d2bbe231bed23de586d0293b51e7a630
Farosh <46266489+PhotogenicLizard@users.noreply.github.com>
2022-12-04 14:19:05 -0600

    updated todolist

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
 from welcomemenu import Ui_WelcomeMenu
+from controller import *
+"""
+need to run program from main
+"""
+
+def main():
+    controller = MainController()
 
 
 if __name__ == "__main__":
+
+    """
     app = QApplication(sys.argv)
     welcomemenu = QWidget()
     ui_welcomemenu = Ui_WelcomeMenu()
     ui_welcomemenu.setupUi(welcomemenu)
     welcomemenu.show()
     sys.exit(app.exec_())
+    """
+    print('test')
+    main()
