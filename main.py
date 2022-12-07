cf60380bfb4e52d24de6a21355d4597e40c8b70c
Farosh <46266489+PhotogenicLizard@users.noreply.github.com>
2022-12-07 14:18:03 -0600

    updated method to run from main

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 import sys
-
 from PyQt5.QtWidgets import QApplication, QWidget
-
 from welcomemenu import Ui_WelcomeMenu
 from controller import *
-"""
-need to run program from main
-"""
 
 def main():
-    controller = MainController()
-
+    app = QApplication(sys.argv)
+    _ = MainController()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
-
-    """
-    app = QApplication(sys.argv)
-    welcomemenu = QWidget()
-    ui_welcomemenu = Ui_WelcomeMenu()
-    ui_welcomemenu.setupUi(welcomemenu)
-    welcomemenu.show()
-    sys.exit(app.exec_())
-    """
-    print('test')
     main()
