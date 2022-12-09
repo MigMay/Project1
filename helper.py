5e103f3e582afc0258bab5f1c16a305d26c557fe
Farosh <46266489+PhotogenicLizard@users.noreply.github.com>
2022-12-09 14:05:41 -0600

    added docstrings

--- helper.py
+++ helper.py
@@ -3,11 +3,23 @@ from results import *
 from PyQt5 import QtWidgets
 
 def logVote(vote_list):
+    """
+    adds the votes to the csv file
+
+    :param vote_list: a list containing the votes
+    :return: none
+    """
     with open('votingrecords.csv', 'a', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(vote_list)
 
 def readVotes():
+    """
+    adds the votes to the csv file
+
+    :param vote_list: a list containing the votes
+    :return: none
+    """
     CA_vote = 0
     CB_vote = 0
     CC_vote = 0
